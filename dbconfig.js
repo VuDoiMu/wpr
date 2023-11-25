fce17d4568a3e26d0eba584a8e5975d273f56da0
HomePC <trinhlongvu153@gmail.com>
2023-11-25 15:23:35 +0700

    update

--- dbconfig.js
+++ dbconfig.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2");
+import mysql from "mysql2";
 
 const dbConfig = {
   host: "localhost",
@@ -28,3 +28,5 @@ initSQL.forEach((sql) => {
     }
   });
 });
+
+export default db
